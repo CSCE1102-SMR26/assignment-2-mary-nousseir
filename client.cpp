ea550062f7110a2611b710535bd14b133d5ed341
mary <mary.nousseir@aucegypt.edu>
2026-07-05 23:26:44 +0300

    finished assignment

--- client.cpp
+++ client.cpp
@@ -1,8 +1,8 @@
 #include "client.h"
 #include "ui_client.h"
 #include <QTcpSocket>
-#include <QCoro/QCoroNetwork> // Required to await QTcpSocket
-#include <QMessageBox>        // Required for UI feedback
+#include <QCoroNetwork>
+#include <QMessageBox>
 #include <QDebug>
 
 client::client(QWidget *parent)
@@ -17,29 +17,28 @@ client::~client()
     delete ui;
 }
 
+void client::on_sendButton_clicked()
+{
+    QString host = "127.0.0.1";
+    quint16 port = 54321;
+    QString message = ui->messagelineEdit->text();
+
+    if (!message.isEmpty()) {
+        sendNetworkMessage(host, port, message);
+    }
+}
+
 QCoro::Task<void> client::sendNetworkMessage(QString host, quint16 port, QString message)
 {
-    // TODO: Create a QTcpSocket locally. (It will be destroyed automatically when this function ends).
+    QTcpSocket socket;
 
-    // TODO: Print a qDebug() message stating you are attempting to connect to the host and port.
-
-    // TODO: Initiate the connection to the host using the socket.
-
-    // TODO: Asynchronously wait for the connection to establish using co_await and qCoro.
-    // Save the result into a boolean variable (e.g., 'connected').
-
-    // TODO: Write an if-statement. If NOT connected:
-    // 1. Show a QMessageBox::critical stating the connection failed, using socket.errorString().
-    // 2. co_return; to exit early.
-
-    // TODO: Print a qDebug() message confirming connection.
-
-    // TODO: Write the 'message' variable to the socket (remember to convert it to Utf8).
-
-    // TODO: Tell Qt to gracefully disconnect from the host.
-
-    // TODO: Asynchronously wait for Qt to finish flushing the buffer and disconnecting using co_await and qCoro.
-
-    // TODO: Show a QMessageBox::information stating the message was sent successfully.
-
+co_await qCoro(socket).connectToHost(host, port);
+    if (socket.isOpen()) {
+        socket.write(message.toUtf8());
+        ui->messagelineEdit->clear();
+        QMessageBox::information(this, "Success", "Message sent to server!");
+    } else {
+        qDebug() << "Could not connect to server";
+        QMessageBox::critical(this, "Error", "Make sure the server is running!");
+    }
 }
--- client.h
+++ client.h
@@ -15,10 +15,10 @@ public:
     client(QWidget *parent = nullptr);
     ~client();
 
+private slots:
+    void on_sendButton_clicked();
 private:
     Ui::client *ui;
-
-    // The coroutine that will handle the network operation without blocking the UI
     QCoro::Task<void> sendNetworkMessage(QString host, quint16 port, QString message);
 };
 
